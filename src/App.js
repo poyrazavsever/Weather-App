26d5b2691bf7a5ab2c21c1ac17267ccfcbc9bb78
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-04-25 21:37:26 +0300

    feat: add route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import axios from "axios";
-import { useState } from "react";
+import {Routes, Route} from "react-router-dom"
+
 
 //Page
 import Home from "./pages/Home/Home";
+import WeatherPage from "./pages/Weather/WeatherPage";
 
 function App() {
 
-  const [weather, setWeather] = useState();
-
-  const getWeatherData = async (lat, lon) => {
-      try{
-        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ee842d38462534a74ced2a9a88bf2a37`)
-        setWeather(data)
-      } catch {
-        console.log("Veri alınırken bir hata oluştu")
-      }
-      
-  }
-
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cities/:id" element={<WeatherPage />} />
+      </Routes>
     </div>
   );
 }
