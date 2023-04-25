a1fabe43cc95b4c8bf789c03d7acd2b6db8cf06f
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-04-25 21:48:42 +0300

    feat: change button and add alert box

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Helmet } from "react-helmet";
 
 // components
@@ -8,7 +8,7 @@ import Map from './Map';
 function Home() {
 
     const [selectList, setSelectList] = useState(1)
-    
+
     return (
         <div className='max-w-screen-lg mx-auto'>
 
@@ -20,10 +20,14 @@ function Home() {
                 <h1 className='text-3xl sm:text-5xl md:text-7xl text-white font-bold text-center my-16'>Türkiye Hava Durumu</h1>
             </div>
 
-            <button onClick={() => selectList === 1 ? setSelectList(2) : setSelectList(1)}
-            className='text-indigo-200 text-center flex justify-center items-center border border-indigo-200 px-6 py-2'>
-                {selectList === 1 ? "Haritaya Geç" : "Şehir Listesine Geç"}
-            </button>
+            <div className='w-full flex justify-center'>
+                <button onClick={() => selectList === 1 ? setSelectList(2) : setSelectList(1)}
+                    className='text-indigo-200 text-center flex justify-center items-center border border-indigo-200 px-6 py-2 hover:bg-indigo-200 hover:text-indigo-950 transition-all duration-100'>
+                    {selectList === 1 ? "Haritaya Geç" : "Şehir Listesine Geç"}
+                </button>
+            </div>
+
+
 
             <div className={selectList === 2 ? "hidden" : "visible"}>
                 <CityList />
--- src/pages/Home/Map.jsx
+++ src/pages/Home/Map.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setPlateNumber } from "../../store/counterSlice"
 import { NavLink } from 'react-router-dom'
 
+import {CgDanger} from "react-icons/cg"
+
 
 
 function Map() {
@@ -12,16 +14,37 @@ function Map() {
   const plateNumber = useSelector((state) => state.counter.plateNumber)
 
   return (
-    <NavLink to={`/cities/${plateNumber}`}>
-      <TurkeyMap
-        hoverable={true}
-        customStyle={{ idleColor: "#4f46e5", hoverColor: "#a5b4fc" }}
-        showTooltip={true}
-        onClick={(city) => {
-          dispatch(setPlateNumber(city.plateNumber))
-        }}
-      />
-    </NavLink>
+
+    <div>
+
+      {/* Alert Message  */}
+
+      <div class="flex items-start p-4 my-12 text-sm text-red-400 rounded-lg bg-gray-800" role="alert">
+        <CgDanger className='text-xl mr-2'/>
+        <div>
+          <span class="font-medium">Haritada düzeltilecek hatalar;</span>
+          <ul class="mt-1.5 ml-4 list-disc list-inside">
+            <li>İlk bastığınız şehirde null değer döndürüyor.</li>
+            <li>İkinci basılan şehirin açılmasına karşın sonrasında tıklanıldığında tekrar ikinci açılıyor. Ve bu döngü halinde devam ediyor.</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Map  */}
+
+      <NavLink to={`/cities/${plateNumber}`}>
+        <TurkeyMap
+          hoverable={true}
+          customStyle={{ idleColor: "#4f46e5", hoverColor: "#a5b4fc" }}
+          showTooltip={true}
+          onClick={(city) => {
+            dispatch(setPlateNumber(city.plateNumber))
+          }}
+        />
+      </NavLink>
+
+    </div>
+
   )
 }
 
